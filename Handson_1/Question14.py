07bf7537f36828ca09daeb6372a077d0c9e01616
Malavicka V <malavicka7@gmail.com>
2026-06-02 21:44:24 +0530

    Added python program

--- Handson_1/Question14.py
+++ Handson_1/Question14.py
@@ -1,9 +1,13 @@
 total_price = 0
+
 while True:
     price = int(input("Enter the price of the item: "))
     quantity = int(input("Enter the quantity of the item: "))
+
     total_price = total_price + (price * quantity)
-    choice = input("Do you want to enter another item? (yes/no): ").lower()
+
+    choice = input("Do you want to enter another item? (yes/no): ").strip().lower()
+
     if choice == "no":
         break
 
